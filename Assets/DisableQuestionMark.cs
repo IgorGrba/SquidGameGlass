6b9fb3da7a0f49fa0f406f87792ee977cc675c1f
Igor Grba <72897578+LogerzGit@users.noreply.github.com>
2021-10-04 14:53:15 +0200

    cards disable

---
+++ Assets/DisableQuestionMark.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableQuestionMark : MonoBehaviour
+{
+    [SerializeField] private GameObject checkMark;
+    [SerializeField] private GameObject checkMark2;
+
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            checkMark.SetActive(false);
+            checkMark2.SetActive(false);
+        }
+    }
+}
